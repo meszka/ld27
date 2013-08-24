1749cb968f6425e450d424fe3d3cdd10ca202a26
Jakub Szwachła <meszkax@gmail.com>
2013-08-24 21:01:56 +0200

    Hunger, bush positions from map

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var Game = function () {
 
-    var player, walk, map, viewport, bushes;
+    var player, walk, map, viewport, bushes, time;
 
     function Player(options) {
         jaws.Sprite.call(this, options);
@@ -23,21 +23,34 @@ var Game = function () {
         }
     };
 
+    Player.prototype.eat = function (amount) {
+        this.hunger -= amount;
+        if (this.hunger < 0) { this.hunger = 0; }
+    }
+
     Player.prototype.interact = function () {
+        var that = this;
         bushes.forEach(function (bush) {
-            if (nextTo(player, bush, 1)) {
-                if (bush.berries) { bush.takeBerry(); }
+            if (nextTo(that, bush, 1)) {
+                if (bush.berries) {
+                    bush.takeBerry();
+                    that.eat(1);
+                }
             }
         });
     };
 
     function Bush(options) {
         jaws.Sprite.call(this, options);
-        this.berries = 5;
-        this.setImage(this.sheet.frames[0]);
+        this.setAnchor('bottom_left');
+        this.spawnBerries();
     }
     inherits(Bush, jaws.Sprite);
 
+    Bush.prototype.spawnBerries = function () {
+        this.berries = 5;
+        this.setImage(this.sheet.frames[0]);
+    };
     Bush.prototype.takeBerry = function () {
         if (this.berries) {
             this.berries -= 1;
@@ -48,6 +61,15 @@ var Game = function () {
         image: 'berry_bush.png',
         frame_size: [8, 8]
     });
+
+    function tiledSpawnBushes(data) {
+        var bushes = new jaws.SpriteList();
+        data.layers[1].objects.forEach(function (object) {
+            var bush = new Bush({ x: object.x, y: object.y });
+            bushes.push(bush);
+        });
+        return bushes;
+    }
 
     function nextTo(obj1, obj2, distance) {
         var larger_rect = obj1.rect();
@@ -62,6 +84,17 @@ var Game = function () {
         });
     }
 
+    function newDay() {
+        time = 10 * 1000;
+        player.x = 100;
+        player.y = 75;
+        player.hunger = 20;
+
+        bushes.forEach(function (bush) {
+            bush.spawnBerries();
+        });
+    }
+
     this.setup = function () {
         jaws.canvas.width = jaws.canvas.width * 4;
         jaws.canvas.height = jaws.canvas.height * 4;
@@ -69,6 +102,7 @@ var Game = function () {
         jaws.useCrispScaling();
 
         map = tiledInitMap(jaws.assets.get('map.json'));
+        bushes = tiledSpawnBushes(jaws.assets.get('map.json'));
 
         walk = new jaws.Animation({
             sprite_sheet: 'front_walk.png',
@@ -79,11 +113,9 @@ var Game = function () {
         player = new Player({ x: 100, y: 75, width: 4, height: 4 });
         player.anim = walk;
 
-        bushes = new jaws.SpriteList();
-        var bush = new Bush({ width: 8, height: 8, x: 150, y: 90 });
-        bushes.push(bush);
-
         viewport = new jaws.Viewport({ max_x: map.width, max_y: map.height });
+
+        newDay();
     };
 
     this.update = function () {
@@ -93,6 +125,11 @@ var Game = function () {
         if (jaws.pressed('up'))    { player.move(0, -1); }
         if (jaws.pressed('down'))  { player.move(0, 1); }
         if (jaws.pressedWithoutRepeat('z'))  { player.interact(); }
+
+        time -= jaws.game_loop.tick_duration;
+        if (time <= 0) {
+            newDay();
+        }
     };
 
     this.draw = function () {
@@ -108,6 +145,16 @@ var Game = function () {
             bushes.draw();
             player.draw();
         });
+
+        jaws.context.font = '10px monospace';
+        jaws.context.fillStype = 'rgb(255,255,255)';
+        jaws.context.fillText((Math.ceil(time / 1000)), 20, 20);
+        if (player.hunger > 0) {
+            jaws.context.fillText('Hungry!', 50, 20);
+        }
+        if (time < 4 * 1000) {
+            jaws.context.fillText('Need sleep!', 50, 30);
+        }
     };
 
 };
